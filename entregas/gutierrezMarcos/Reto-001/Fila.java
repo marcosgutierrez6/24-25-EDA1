1bb636cfa03d2f4b8f9bf0ea487f25cccd834d62
Marcos Gutiérrez <144948806+marcosgutierrez6@users.noreply.github.com>
2026-03-05 13:13:13 +0100

    Fix: Correcciones de estandares

--- entregas/gutierrezMarcos/Reto-001/Fila.java
+++ entregas/gutierrezMarcos/Reto-001/Fila.java
@@ -4,60 +4,58 @@ public class Fila {
 
     private Cliente[] clientes;
     private int posicionUltimo;
+    private final Random random;
 
     public Fila(){
-
         clientes = new Cliente[200];
         posicionUltimo = 0;
-
+        random = new Random();
     }
 
-    public void añadirCliente(){
-
-        Random random = new Random();
+    public void añadirClientesAleatorios(){
         int numClientes = random.nextInt(3) + 1;
     
         for (int i = 0; i < numClientes; i++) {
             if (posicionUltimo < clientes.length) {
-            clientes[posicionUltimo] = new Cliente();
-            posicionUltimo++;
+                clientes[posicionUltimo] = new Cliente();
+                posicionUltimo++;
             }
         }
     }
 
     public void atenderCliente(){
         if(hayClientes()){
-            moverIzquierda(clientes, 0, posicionUltimo);
+            moverIzquierda(0, posicionUltimo);
             posicionUltimo--;
         }
     }
 
     private boolean hayClientes() {
-        return clientes[0]!=null;
+        return clientes[0] != null;
     }
 
-    private void moverIzquierda(Cliente[] clientes, int indice, int elementosOcupados){
+    private void moverIzquierda(int indice, int elementosOcupados){
         assert indice >= 0 && indice < elementosOcupados;
 
-        for (int i = indice; i < elementosOcupados; i++) {
+        for (int i = indice; i < elementosOcupados - 1; i++) {
             clientes[i] = clientes[i + 1];
         }
 
-        clientes[elementosOcupados] = null;
-
+        if (elementosOcupados > 0) {
+            clientes[elementosOcupados - 1] = null;
+        }
     }
 
     public void clienteSeVa(){
-        assert posicionUltimo>0;
-        moverIzquierda(clientes, new Random().nextInt(posicionUltimo), posicionUltimo);
+        assert posicionUltimo > 0;
+        moverIzquierda(random.nextInt(posicionUltimo), posicionUltimo);
         posicionUltimo--;
-        
     }
 
     public void recibirItemsExternos() {
-        if(posicionUltimo > 0 && new Random().nextInt(100) < 20){ 
-            int clienteSeleccionado = new Random().nextInt(posicionUltimo); 
-            int itemsExtra = 2 + new Random().nextInt(4);
+        if(posicionUltimo > 0 && random.nextInt(100) < 20){ 
+            int clienteSeleccionado = random.nextInt(posicionUltimo); 
+            int itemsExtra = 2 + random.nextInt(4);
             clientes[clienteSeleccionado].añadirItems(itemsExtra);
         }
     }
@@ -69,12 +67,13 @@ public class Fila {
     public void moverDerecha(int posicion) {
         assert posicion >= 0 && posicion < clientes.length; 
         assert posicionUltimo < clientes.length;
-        for (int i = posicionUltimo; i >= posicion; i--) {
+        
+        int inicioMovimiento = Math.min(posicionUltimo, clientes.length - 2);
+        
+        for (int i = inicioMovimiento; i >= posicion; i--) {
             clientes[i + 1] = clientes[i]; 
         }
         posicionUltimo++;
         clientes[posicion] = new Cliente();
-        
     }
-    
 }
